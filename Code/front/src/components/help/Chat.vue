c3be940cb68f0b0c40aac47dab3bd66d236ab4fd
Cristian David Hernandez Suarez <69637534+cristian-hernandez20@users.noreply.github.com>
2022-11-14 02:52:10 -0500

    hrllo

---
+++ Code/front/src/components/help/Chat.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-dialog v-model="estado" max-width="840px" max-height="500">
+    <v-card>
+      <v-footer class="sticky" color="primary">
+        <h3 class="white--text">CHAT IZZI-PARKING</h3>
+        <v-spacer></v-spacer>
+        <v-btn @click="estado = false">salir</v-btn>
+      </v-footer>
+      <div>
+        <iframe src="http://localhost:3001/login" height="500" containe width="840"></iframe>
+      </div>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      estado: true,
+    };
+  },
+};
+</script>
+
+<style>
+.sticky {
+  position: sticky;
+  top: 0px;
+  z-index: 1;
+}
+.scroll {
+  overflow-x: scroll;
+  overflow-y: hidden;
+}
+</style>
--- Code/front/src/views/user/Helps.vue
+++ Code/front/src/views/user/Helps.vue
@@ -2,46 +2,67 @@
   <v-container fluid>
     <v-row justify="center" class="pt-4 ml-16 mr-2">
       <v-col cols="12" lg="12" sm="12" md="12">
-          <v-card elevation="4">
-            <v-card-text class="text-title primary--text mt-0">
-              <h2 class="text-start">
-                <v-icon color="primary" class="mb-1">mdi-video</v-icon>
-                Ayuda > Video guias
-              </h2>
-            </v-card-text>
-          </v-card>
+        <v-card elevation="4">
+          <v-card-text class="text-title primary--text mt-0">
+            <h2 class="text-start">
+              <v-icon color="primary" class="mb-1">mdi-video</v-icon>
+              Ayuda > Video guias
+            </h2>
+          </v-card-text>
+        </v-card>
       </v-col>
-      <v-col cols="6" lg="6" sm="6" md="6" v-for=" (item , i) in data" :key="i">
+      <v-col cols="6" lg="6" sm="6" md="6" v-for="(item, i) in data" :key="i">
         <v-card elevation="12">
           <v-card-text class="primary--text">
             <v-list>
               <v-list-item>
                 <v-list-item-content>
                   <v-list-item-title class="text-title">
-                    <v-chip  color="primary">
-                      {{ item.title}} 
+                    <v-chip color="primary">
+                      {{ item.title }}
                     </v-chip>
                   </v-list-item-title>
                 </v-list-item-content>
               </v-list-item>
               <v-divider></v-divider>
               <v-list-item class="ml-0">
-                <v-list-item-title class="link"> <iframe width="500" height="250" :src="item.video" title="Cómo registrarse e iniciar sesión" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> </v-list-item-title>
+                <v-list-item-title class="link">
+                  <iframe
+                    width="500"
+                    height="250"
+                    :src="item.video"
+                    title="Cómo registrarse e iniciar sesión"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen
+                  ></iframe>
+                </v-list-item-title>
               </v-list-item>
-             
+
               <v-list-item class="ml-0">
-                <v-list-item-title class="text-title">  {{item.descrip}} </v-list-item-title>
+                <v-list-item-title class="text-title"> {{ item.descrip }} </v-list-item-title>
               </v-list-item>
             </v-list>
           </v-card-text>
         </v-card>
       </v-col>
+      <v-tooltip left>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn fab color="primary" fixed bottom right v-bind="attrs" v-on="on"> <v-icon>mdi-forum-outline </v-icon> </v-btn>
+        </template>
+        <span>Chatea con un asesor</span>
+      </v-tooltip>
     </v-row>
+    <Chat />
   </v-container>
 </template>
 <script>
+import Chat from "../../components/help/Chat.vue";
 export default {
   name: "help",
+  components: {
+    Chat,
+  },
   data() {
     return {
       data: [
@@ -70,4 +91,10 @@ export default {
   },
 };
 </script>
-  
+<style>
+.sticky {
+  position: absolute;
+  top: 0px;
+  z-index: -1;
+}
+</style>
